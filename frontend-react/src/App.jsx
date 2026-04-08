07c2558e3aeedbe40b29318c38d19cb67cd51ef3
Thompson Edward <AugustJnr@AugustJnr.local>
2026-04-09 00:13:32 +0100

    Homepage React

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import './assets/css/styles.css'
+import Header from './components/Header'
+import Home from './components/Home'
+import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Header />
+      <Home />
+      <Footer />
     </>
   )
 }
---
+++ frontend-react/src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Button from './common/Button'
+
+function Home() {
+  return (
+    <>
+       <div className="container">
+        <div className="p-3 text-center bg-light-dark rounded">
+            <h1 className='text-light'>Stock Prediction Portal</h1>
+            <p className="text-light lead">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quo fugiat fugit voluptatibus! Repellat voluptatum quam doloremque pariatur! Ipsa molestias laboriosam quisquam alias est quasi deleniti laborum autem numquam tempora!
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque quia, officiis voluptatum consequatur dolores, atque veniam error a iste ducimus pariatur cum obcaecati rerum. Corporis dolore tempora natus maxime nemo!
+            </p>
+            <Button class='btn-info' text='Login' />
+        </div>
+       </div>
+    </>
+  )
+}
+
+export default Home
